c2a6473237eb3ca8d0ca5e9fbc4a125aba814ce6
TiffanytorinoHe <45566238+TiffanytorinoHe@users.noreply.github.com>
2020-06-24 13:23:19 +0800

    initial

---
+++ dataset.py
@@ -0,0 +1,62 @@
+import torch.utils.data as data
+import PIL.Image as Image
+import numpy as np
+import torch
+
+
+# data.Dataset:
+
+# 所有子类应该override__len__和__getitem__，前者提供了数据集的大小，后者支持整数索引，范围从0到len(self)
+
+path="data/VOC2012/"
+
+class VOCDataset(data.Dataset):
+    def __init__(self, train: bool, transform=None, label_transform=None):
+        self.train = train
+        self.transform = transform
+        self.label_transform = label_transform
+        # 标签中RGB颜色的值
+        self.voc_colors = [[0, 0, 0], [128, 0, 0], [0, 128, 0], [128, 128, 0],
+                           [0, 0, 128], [128, 0, 128], [0, 128, 128], [128, 128, 128],
+                           [64, 0, 0], [192, 0, 0], [64, 128, 0], [192, 128, 0],
+                           [64, 0, 128], [192, 0, 128], [64, 128, 128], [192, 128, 128],
+                           [0, 64, 0], [128, 64, 0], [0, 192, 0], [128, 192, 0],
+                           [0, 64, 128]]
+        '''
+        颜色对应的类别
+        self.voc_classes = ['background', 'aeroplane', 'bicycle', 'bird', 'boat',
+                            'bottle', 'bus', 'car', 'cat', 'chair', 'cow',
+                            'dining table', 'dog', 'horse', 'motorbike', 'person',
+                            'potted plant', 'sheep', 'sofa', 'train', 'tv/monitor']
+        '''
+        self.color2class = np.zeros((8, 8, 8), dtype=int)
+        for i, color in enumerate(self.voc_colors):
+            self.color2class[int(color[0] / 32)][int(color[1] / 32)][int(color[2] / 32)] = i
+        if train:
+            self.imgs = np.loadtxt('./VOC2012/ImageSets/Segmentation/train.txt', dtype=str)
+        else:
+            self.imgs = np.loadtxt('./VOC2012/ImageSets/Segmentation/val.txt', dtype=str)
+
+    def __getitem__(self, index):
+        img_name = self.imgs[index]
+        origin_img = Image.open('./VOC2012/JPEGImages/' + str(img_name) + '.jpg').convert('RGB')
+        label_img = Image.open('./VOC2012/SegmentationClass/' + str(img_name) + '.png').convert('RGB')
+
+        if self.transform is not None:
+            origin_img = self.transform(origin_img)
+            label_img = self.label_transform(label_img)
+
+        if self.train:
+            label = torch.zeros(label_img.size()[1], label_img.size()[2], dtype=torch.int)
+            for i in range(label.size()[0]):
+                for j in range(label.size()[1]):
+                    class_idx = self.color2class[int(label_img[0][i][j] * 8)][int(label_img[1][i][j] * 8)][
+                        int(label_img[2][i][j] * 8)]
+                    label[i][j] = torch.tensor(class_idx, dtype=torch.long)
+        else:
+            label = label_img
+
+        return origin_img, label
+
+    def __len__(self):
+        return self.imgs.size
---
+++ main.py
@@ -0,0 +1,131 @@
+import torch
+import argparse
+from torch.utils.data import DataLoader
+from torch import nn, optim
+from torchvision.transforms import transforms
+
+from unet import *
+from dataset import VOCDataset
+import utils
+
+
+# 是否使用cuda
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+
+# 超参数
+num_classes = 21  # 分类的类别数
+learning_rate = 1e-4
+batch_size = 8
+model_type = UnetVGG16  # 使用unet中定义的哪个模型
+scale_size = 224  # resnet使用448，vgg使用224
+image_dataset = VOCDataset
+
+# 图像处理
+x_transforms = transforms.Compose([
+    transforms.Resize(scale_size),
+    transforms.CenterCrop(scale_size),
+    transforms.ToTensor(),
+    transforms.Normalize([0.457, 0.439, 0.401], [0.237, 0.233, 0.238])
+])
+
+y_transforms = transforms.Compose([
+    transforms.Resize(224),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+])
+
+
+def train_model(model, criterion, optimizer, dataload):
+    loss_info_list = []
+
+    for epoch in range(start_epoch, end_epoch):
+        print('Epoch {}/{}'.format(epoch, end_epoch - 1))
+        print('-' * 10)
+        dt_size = len(dataload.dataset)
+        epoch_loss = 0
+        step = 0
+        for x, y in dataload:
+            step += 1
+            inputs = x.to(device)
+            labels = y.to(device)
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            # forward
+            outputs = model(inputs)
+            loss = criterion(outputs, labels.long())
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+            print("%d/%d,train_loss:%0.3f" % (step, (dt_size - 1) // dataload.batch_size + 1, loss.item()))
+        epoch_info = "epoch %d loss:%0.3f" % (epoch, epoch_loss/step)
+        loss_info_list.append(epoch_info)
+        print(epoch_info)
+        if epoch % 5 == 0:
+            torch.save(model.state_dict(), 'model/weights_%d.pth' % epoch)
+            torch.save(optimizer.state_dict(), 'model/optim_%d.pth' % epoch)
+
+    with open('result/loss_info.txt', 'a') as f:
+        f.write('\n'.join(loss_info_list))
+
+    return model
+
+
+# 训练模型
+def train(args):
+    model = model_type(num_classes, args.pretrained, args.freeze_encoder).to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam([p for p in model.parameters() if p.requires_grad], lr=learning_rate)
+    try:
+        model.load_state_dict(torch.load(weights_path))
+    except IOError:
+        print('model param not loaded')
+    try:
+        optimizer.load_state_dict(torch.load(optim_path))
+    except IOError:
+        print('optimizer param not loaded')
+    liver_dataset = image_dataset(True, x_transforms, y_transforms)
+    dataloaders = DataLoader(liver_dataset, batch_size=batch_size, shuffle=True, num_workers=16, pin_memory=True)
+    train_model(model, criterion, optimizer, dataloaders)
+
+
+# 显示模型的输出结果
+def test(args):
+    model = model_type(num_classes)
+    model.load_state_dict(torch.load(args.ckpt, map_location='cpu'))
+    dataset = image_dataset(False, x_transforms, y_transforms)
+    dataloaders = DataLoader(dataset, batch_size=1)
+    model.eval()
+    import matplotlib.pyplot as plt
+    plt.ion()
+    with torch.no_grad():
+        for x, label_y in dataloaders:
+            predict_y = model(x)
+            predict_y = utils.get_predict_image(predict_y[0])
+            plt.subplot(1, 2, 1)
+            plt.imshow(predict_y)
+            plt.subplot(1, 2, 2)
+            plt.imshow(transforms.ToPILImage()(label_y[0]))
+            plt.pause(0.01)
+        plt.show()
+
+
+if __name__ == '__main__':
+
+    # 运行参数（用于继续上次的训练）
+    start_epoch = 0
+    end_epoch = 60
+    weights_path =  'model/weights_45.pth'
+    optim_path = 'model/optim_45.pth'
+
+    # 参数解析
+    parse = argparse.ArgumentParser()
+    parse.add_argument("action", type=str, help="train or test")
+    parse.add_argument("pretrained", type=bool, default=False)
+    parse.add_argument("freeze_encoder", type=bool, default=False)
+    parse.add_argument("--ckpt", type=str, help="the path of model weight file")
+    args = parse.parse_args()
+
+    if args.action == "train":
+        train(args)
+    elif args.action == "test":
+        test(args)
